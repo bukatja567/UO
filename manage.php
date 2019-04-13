b25d3e3a5e9d98d302b11ee852e47873f5164a1a
C.C.Tishchenko <constish@gmail.com>
2019-04-14 00:31:56 +0300

    text_fix

--- manage.php
+++ manage.php
@@ -24,6 +24,7 @@ function bot_manager($message, $chat_id, $user_id)
 				if (substr($mess,1,4) == "week") return week($chat_id);
 				if (substr($mess,1,5) == "today") return today();
 				if (substr($mess,1,8) == "tomorrow") return tomorrow();
+				if (substr($mess,1,10) == "professors") return TBPROFESSORS;
 				//if (substr($mess,1,8) == "findp") findp($chat_id);
 			    //----- Команды помошники -----
 			    if (substr($mess,1,7) == "chat_id") sendMessage($chat_id, $chat_id);
