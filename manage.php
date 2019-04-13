3772347675be8d86e913eaa2a2be2a860564448f
C.C.Tishchenko <constish@gmail.com>
2019-04-14 00:27:55 +0300

    group_com_fix

--- manage.php
+++ manage.php
@@ -20,7 +20,7 @@ function bot_manager($message, $chat_id, $user_id)
 			    //----- Умный одногруппник -----
 				if (substr($mess,1,4) == "rasp") raspisan($chat_id);
 				if (substr($mess,1,9) == "headgroup") return TBHEADGROUP;
-				if (substr($mess,1,9) == "group") return TBGROUP;
+				if (substr($mess,1,5) == "group") return TBGROUP;
 				if (substr($mess,1,4) == "week") return week($chat_id);
 				if (substr($mess,1,5) == "today") return today();
 				if (substr($mess,1,8) == "tomorrow") return tomorrow();
