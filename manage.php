bb7d0a5dcc89aa472b26e10967649eb7ab8f9c17
C.C.Tishchenko <constish@gmail.com>
2019-04-17 20:58:00 +0300

    STABLE_V0.9.1

--- manage.php
+++ manage.php
@@ -25,7 +25,7 @@ function bot_manager($message, $chat_id, $user_id)
 				if (substr($mess,1,8) == "nextweek") return nextweek();
 				if (substr($mess,1,5) == "today") return today();
 				if (substr($mess,1,8) == "tomorrow") return tomorrow();
-				if (substr($mess,1,10) == "professors") return TBPROFESSORS;
+				//if (substr($mess,1,10) == "professors") return TBPROFESSORS;
 				if (substr($mess,1,3) == "lab") labccna($chat_id, $mess);
 				if (substr($mess,1,7) == "zapccna") return TBZAPCCNA;
 				//if (substr($mess,1,8) == "findp") findp($chat_id);
@@ -54,7 +54,7 @@ function bot_manager($message, $chat_id, $user_id)
 				if (substr($mess,1,8) == "nextweek") return nextweek();
 				if (substr($mess,1,5) == "today") return today();
 				if (substr($mess,1,8) == "tomorrow") return tomorrow();
-				if (substr($mess,1,10) == "professors") return TBPROFESSORS;
+				//if (substr($mess,1,10) == "professors") return TBPROFESSORS;
 				if (substr($mess,1,3) == "lab") labccna($chat_id, $mess);
 				if (substr($mess,1,7) == "zapccna") return TBZAPCCNA;
 				break;
