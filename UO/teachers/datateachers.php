a869bc994e9099c99777e9e13ba52ad38bfa9bf4
C.C.Tishchenko <constish@gmail.com>
2019-04-19 13:46:41 +0300

    edit datateachers

--- UO/teachers/datateachers.php
+++ UO/teachers/datateachers.php
@@ -7,24 +7,24 @@ function data_TEACHERS()
     $TEACHERS[0]['name_father'] = "Георгиевич";
     $TEACHERS[0]['theme1'] = "Практика";
     $TEACHERS[0]['assessment1'] = "Зачет по практике";
-    $TEACHERS[0]['photo'] = "";
+    $TEACHERS[0]['photo'] = "AgADBAADoagxG-d21FHMr5zubNjOmLAyHxsABHZvZMouXuoAAb04BQABAg";
     $TEACHERS[0]['class'] = "28, 307 аудитории";
     $TEACHERS[0]['theme2'] = "";
     $TEACHERS[0]['assessment2'] = "";
     $TEACHERS[0]['index'] = "";
-    $TEACHERS[0]['constacts'] = "";
+    $TEACHERS[0]['constacts'] = "\n+7(926)525-42-21\nvgleonov@yandex.ru";
 
     $TEACHERS[1]['name'] = "Сергей";
     $TEACHERS[1]['lastname'] = "Парфенов";
     $TEACHERS[1]['name_father'] = "Юрьевич";
     $TEACHERS[1]['theme1'] = "Кросс Платформенное Программирование";
     $TEACHERS[1]['assessment1'] = "Зачет";
-    $TEACHERS[1]['photo'] = "";
+    $TEACHERS[1]['photo'] = "AgADBAADu6gxG3ml1VHL8lbQuxk1mboRIhsABG6ywYbefUVpTZwEAAEC";
     $TEACHERS[1]['class'] = "101-103 аудитории(1 этаж, перед охраной)";
     $TEACHERS[1]['theme2'] = "Информационные системы и технологии";
     $TEACHERS[1]['assessment2'] = "Экзамен";
     $TEACHERS[1]['index'] = "КПП, ИСИТ";
-    $TEACHERS[1]['constacts'] = "";
+    $TEACHERS[1]['constacts'] = "998047@mail.ru";
     
     $TEACHERS[2]['name'] = "Дмитрий";
     $TEACHERS[2]['lastname'] = "Горелко";
@@ -36,7 +36,7 @@ function data_TEACHERS()
     $TEACHERS[2]['theme2'] = "";
     $TEACHERS[2]['assessment2'] = "";
     $TEACHERS[2]['index'] = "Технология программирования Системное администрирование сисадмин"; 
-    $TEACHERS[2]['constacts'] = "";
+    $TEACHERS[2]['constacts'] = "dmitry.gorelko@gmail.com";
     
     $TEACHERS[3]['name'] = "Кирилл";
     $TEACHERS[3]['lastname'] = "Гусин";
@@ -48,19 +48,19 @@ function data_TEACHERS()
     $TEACHERS[3]['theme2'] = "";
     $TEACHERS[3]['assessment2'] = "";
     $TEACHERS[3]['index'] = "CCNA cisco сети";
-    $TEACHERS[3]['constacts'] = ""; 
+    $TEACHERS[3]['constacts'] = "\n+7(916)520-20-72\nkf.gusin@mpgu.edu"; 
 
     $TEACHERS[4]['name'] = "Марина";
     $TEACHERS[4]['lastname'] = "Хецева";
     $TEACHERS[4]['name_father'] = "Сергеевна";
     $TEACHERS[4]['theme1'] = "Элективные курсы по физической культуре";
-    $TEACHERS[4]['assessment1'] = "???";
+    $TEACHERS[4]['assessment1'] = "Зачет";
     $TEACHERS[4]['photo'] = "";
     $TEACHERS[4]['class'] = "КГФ, спортзал";
     $TEACHERS[4]['theme2'] = "";
     $TEACHERS[4]['assessment2'] = "";
     $TEACHERS[4]['index'] = "физ-ра физра физкультура";  
-    $TEACHERS[4]['constacts'] = "";
+    $TEACHERS[4]['constacts'] = "https://vk.com/id5529041";
 
     $TEACHERS[5]['name'] = "Борис";
     $TEACHERS[5]['lastname'] = "Пушкарев";
@@ -72,19 +72,19 @@ function data_TEACHERS()
     $TEACHERS[5]['theme2'] = "";
     $TEACHERS[5]['assessment2'] = "";
     $TEACHERS[5]['index'] = "инглиш английский язык";  
-    $TEACHERS[5]['constacts'] = "";
+    $TEACHERS[5]['constacts'] = "https://vk.com/borris2007";
 
     $TEACHERS[6]['name'] = "Дина";
     $TEACHERS[6]['lastname'] = "Харичева";
     $TEACHERS[6]['name_father'] = "Леонидовна";
     $TEACHERS[6]['theme1'] = "Обмен данными в корпоративных информационных системах (1с)";
     $TEACHERS[6]['assessment1'] = "Зачет";
-    $TEACHERS[6]['photo'] = "";
+    $TEACHERS[6]['photo'] = "AgADBAADuagxG2bV1VFXlzsTRyrg1wj6IRsABMscw60GpG8_5poEAAEC";
     $TEACHERS[6]['class'] = "28 и под ней на втором этаже";
     $TEACHERS[6]['theme2'] = "";
     $TEACHERS[6]['assessment2'] = "";
     $TEACHERS[6]['index'] = "1с";  
-    $TEACHERS[6]['constacts'] = "hdl@mail.ru";
+    $TEACHERS[6]['constacts'] = "\n+7(910)447-58-88\nhdl@mail.ru";
 
     $TEACHERS[7]['name'] = "Юрий";
     $TEACHERS[7]['lastname'] = "Хотунцев";
@@ -96,42 +96,42 @@ function data_TEACHERS()
     $TEACHERS[7]['theme2'] = "";
     $TEACHERS[7]['assessment2'] = "";
     $TEACHERS[7]['index'] = "Экология";  
-    $TEACHERS[7]['constacts'] = "";
+    $TEACHERS[7]['constacts'] = "yul.hotuncev@mpgu.su";
 
     $TEACHERS[8]['name'] = "Кермен";
     $TEACHERS[8]['lastname'] = "Гоголданова";
     $TEACHERS[8]['name_father'] = "Вячеславовна";
     $TEACHERS[8]['theme1'] = "Виртуальные измерительные приборы";
     $TEACHERS[8]['assessment1'] = "Зачет";
-    $TEACHERS[8]['photo'] = "";
+    $TEACHERS[8]['photo'] = "AgADBAADkKgxG-3j1FFBJIgJV1Md5khUIBsABKKP4wxxcPR95DkFAAEC";
     $TEACHERS[8]['class'] = "Электротехника";
     $TEACHERS[8]['theme2'] = "";
     $TEACHERS[8]['assessment2'] = "";
     $TEACHERS[8]['index'] = "ВИП";  
-    $TEACHERS[8]['constacts'] = "";
+    $TEACHERS[8]['constacts'] = "kv.gogoldanova@mpgu.su";
     
     $TEACHERS[9]['name'] = "Ольга";
     $TEACHERS[9]['lastname'] = "Поликашина";
     $TEACHERS[9]['name_father'] = "Владимировна";
     $TEACHERS[9]['theme1'] = "Правоведение";
     $TEACHERS[9]['assessment1'] = "Зачет";
-    $TEACHERS[9]['photo'] = "";
+    $TEACHERS[9]['photo'] = "AgADBAAD9qgxG3tQzVHW91KGEm0KJi8xsRoABKZdUfvVAjnoa30AAgI";
     $TEACHERS[9]['class'] = "12";
     $TEACHERS[9]['theme2'] = "";
     $TEACHERS[9]['assessment2'] = "";
     $TEACHERS[9]['index'] = "";  
-    $TEACHERS[9]['constacts'] = "";
+    $TEACHERS[9]['constacts'] = "ov.polikashina@mpgu.su";
 
     $TEACHERS[10]['name'] = "Борис";
     $TEACHERS[10]['lastname'] = "Рябов";
     $TEACHERS[10]['name_father'] = "Алексеевич";
     $TEACHERS[10]['theme1'] = "Современное производство и окружающая среда";
     $TEACHERS[10]['assessment1'] = "Зачет";
-    $TEACHERS[10]['photo'] = "";
+    $TEACHERS[10]['photo'] = "AgADBAADz6gxG8W_1FHm98xQ1DXem-_jIRsABDoxg24MLaCA_JgEAAEC";
     $TEACHERS[10]['class'] = "Электротехника, рядом и 4 аудитория";
     $TEACHERS[10]['theme2'] = "";
     $TEACHERS[10]['assessment2'] = "";
     $TEACHERS[10]['index'] = "Экология";  
-    $TEACHERS[10]['constacts'] = "";
+    $TEACHERS[10]['constacts'] = "\n+7(910)444-20-94\nba.ryabov@mpgu.su";
     }
 ?>
